3b9b55ac4b1e87253a4501a19d218bd1d0819bd7
GuoLin <guolin@guolin-ubuntu.(none)>
2010-08-04 03:33:25 +0800

    dos2unit

--- doc/zh_CN/generate.sh
+++ doc/zh_CN/generate.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 ronn --fragment api.markdown
-cat api_header.html api.1.html_fragment api.footer.html > api.html
+cat api_header.html api.1.html_fragment api_footer.html > api.html
 
 echo api.html has been generated.
